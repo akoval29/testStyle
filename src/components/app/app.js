8b223a95fbb30ad1e06030e882a46945a4ae6739
akoval29 <akoval29@gmail.com>
2022-05-25 00:57:02 +0300

    v1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-
+import styled from 'styled-components';
 import './app.css';
+
 
 // Передача аргументів - Варіант № 1
 function WhoAmI1 (props) {
@@ -76,6 +77,15 @@ class WhoAmI5 extends Component {
     }))
   }
 
+  //событие инпута. передали в сетстейт обьект, изменили стейт.
+  commitInputOnChanges = (e, color) => {
+    console.log(color);
+    console.log(e.target);
+    console.log(e.target.value);
+    this.setState({
+      pos: e.target.value
+    })
+  }
 
   render() {
     const {name, lastname, link} = this.props;
@@ -91,7 +101,6 @@ class WhoAmI5 extends Component {
         
         {/* Третий метод */}
         <button onClick={() => this.nextYear2()} >{this.state.textPlus}</button>
-
 
         <h1>My name is {name}, 
         lastname - {lastname}, 
@@ -111,183 +120,15 @@ class WhoAmI5 extends Component {
   }
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        {name: 'Malcolm Corley', salary: 600, increase: false, rise: true, id: 1},
-        {name: 'Mary Corley', salary: 700, increase: true, rise: false, id: 2},
-        {name: 'Uncle Tork', salary: 850, increase: false, rise: false, id: 3},
-        {name: 'Ben', salary: 500, increase: false, rise: false, id: 4},
-      ],
-      term: "",
-      filter: "all"
-    }
-    this.maxId = 5;
-  }
-
-  deleteItem = (id) => {
-    this.setState(({data}) => {
-
-      // неудобний метод =) створюєм новий масив, де буде копія data без елемента, визначеного індексом, топто того що має бути видаленим
-      // const index = data.findIndex(elem => elem.id === id);
-      // const before = data.slice(0, index);
-      // const after = data.slice(index +1);
-      // const newArr = [...before, ...after];
-      // return {
-      //   data: newArr
-      // }
-
-      // удобний метод
-      return {
-        data: data.filter(item => item.id !== id)
-      }
-    })
-  }
-
-  //Добавить нового сотрудника
-  addItem = (name, salary) => {
-    const newItem = {
-      name,
-      salary,
-      increase: false,
-      rise: false,
-      id: this.maxId++
-    }
-    this.setState(({data}) => {
-      if (newItem.salary <= 0 || newItem.salary ==="" || newItem.name.length <= 2) {
-      console.log("Неверные данные");
-      } else {
-        return {
-          data: [...data, newItem]
-      }      
-      }
-    });
-  }
-
-  // заставляем работать печеньки и звёздочки (ВАРИАНТ 1: ищем индекс нужного элемента, делаем массив из кусков)
-  // onToggleIncrease = (id) => {
-  //   this.setState(({data}) => {
-  //     const index = data.findIndex(elem => elem.id === id);
-  //     const old = data[index];
-  //     const newItem = {...old, increase: !old.increase};
-  //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-
-  //     return {
-  //       data: newArr
-  //     }
-  //   })
-  // }
-
-  // onToggleRise = (id) => {
-  //   this.setState(({data}) => {
-  //     const index = data.findIndex(elem => elem.id === id);
-  //     const old = data[index];
-  //     const newItem = {...old, rise: !old.rise};
-  //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-
-  //     return {
-  //       data: newArr
-  //     }
-  //   })
-  // }
-
-    // заставляем работать печеньки и звёздочки (ВАРИАНТ 2 - МАПим, тоесть создаем новый масив с условием и в нем ретурнится тогл)
-  // onToggleIncrease = (id) => {
-  //   this.setState(({data}) => ({
-  //     data: data.map(item => {
-  //       if (item.id ===id ) {
-  //         return {...item, increase: !item.increase}
-  //       }
-  //       return item;
-  //     })
-  //   }))
-  // }
-
-  // onToggleRise = (id) => {
-  //   this.setState(({data}) => ({
-  //     data: data.map(item => {
-  //       if (item.id ===id ) {
-  //         return {...item, rise: !item.rise}
-  //       }
-  //       return item;
-  //     })
-  //   }))
-  // }
-
-    //заставляем работать печеньки и звёздочки (ВАРИАНТ 3 - укорачиваем Вариант 2)
-    onToggleProp = (id, prop) => {
-    this.setState(({data}) => ({
-      data: data.map(item => {
-        if (item.id ===id ) {
-          return {...item, [prop]: !item[prop]}
-        }
-        return item;
-      })
-    }))
-  }
-
-  searchEmp = (items, term) => {
-    if (term.length===0) {
-      return items;
-    }
-    return items.filter(item => {
-      return item.name.indexOf(term) > -1
-    })
-  }
-
-  onUpdateSearch = (term) => { 
-    this.setState({term: term})  //можна так
-    // this.setState({term})  укорочений варіант
-  }
-
-  filterPost = (items, filter) => {
-    switch (filter) {
-      case "rise": return items.filter(item => item.rise);  //це аналогічно - item => if (item.rise) return ...
-      case "zp1000+": return items.filter(item => item.salary > 1000);
-      default: return items
-    }
-  }
-
-  onFilterSelect = (filter) => {
-    this.setState({filter})
-  }
-
-
-  render() {
-    const {data, term, filter} = this.state;
-    const employees = this.state.data.length;
-    const increased = this.state.data.filter(item => item.increase).length;
-    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
-
-    return (
-      <div className="app">
-        <AppInfo 
-          employees = {employees} 
-          increased = {increased}/>
-        <div className="search-panel">
-          <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-          <AppFilter 
-            filter={filter} 
-            onFilterSelect={this.onFilterSelect}/>
-        </div>
-        
-        <EmployeesList 
-          data={visibleData}
-          onDelete={this.deleteItem}
-          onToggleProp = {this.onToggleProp}/>
-        
-        <EmployeesAddForm 
-          onAdd={this.addItem}/>
-        
-        <WhoAmI1 name="John" lastname="Smith" link="facebook.com"/>
-        <WhoAmI2 name="Vin" lastname="Diesel" link="facebook.com"/>
-        <WhoAmI3 name={{firstName: "Part of"}} lastname="Object" link="facebook.com"/>
-        <WhoAmI4 name={() => {return "Function here !!!!!"}} lastname="realy!" link="facebook.com"/>
-        <WhoAmI5 name="we use classes!!!" lastname="here!!" link="facebook.com"/>
-      </div>
-    );
-  }
-};
+function App() {
+  return (
+    <div className="app">
+    <WhoAmI1 name="John" lastname="Smith" link="facebook.com"/>
+    <WhoAmI2 name="Vin" lastname="Diesel" link="facebook.com"/>
+    <WhoAmI3 name={{firstName: "Part of"}} lastname="Object" link="facebook.com"/>
+    <WhoAmI4 name={() => {return "Function here !!!!!"}} lastname="realy!" link="facebook.com"/>
+    <WhoAmI5 name="we use classes!!!" lastname="here!!" link="facebook.com"/>
+  </div>
+  );
+}
 export default App;
